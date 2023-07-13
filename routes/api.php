c1a38bc0f1128efe21c6880847725905a4b3e6aa
Mateus Lima <dev.org.ml@gmail.com>
2023-07-13 14:54:56 -0300

    refactor: improve api routes file

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,11 @@ use App\Http\Controllers\V1\{
 };
 use Illuminate\Support\Facades\Route;
 
-Route::get('/courses', [CourseController::class, 'index']);
-Route::post('/courses', [CourseController::class, 'store']);
-Route::get('/courses/{uuid}', [CourseController::class, 'show']);
+Route::name('courses.')->prefix('courses')->group(function () {
+    Route::get('/', [CourseController::class, 'index'])->name('index');
+    Route::post('/', [CourseController::class, 'store'])->name('store');
+    Route::get('/{uuid}', [CourseController::class, 'show'])->name('show');
+});
 
 Route::get('/', function () {
     return response()->json([
