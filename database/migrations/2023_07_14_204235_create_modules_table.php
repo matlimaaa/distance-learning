3771b34cbd92e4f26e1ab23ca0cf2ff85c54b149
Mateus Lima <dev.org.ml@gmail.com>
2023-07-17 19:35:21 -0300

    fix: add onDelete to modules table

--- database/migrations/2023_07_14_204235_create_modules_table.php
+++ database/migrations/2023_07_14_204235_create_modules_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('modules', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('course_id')->constrained();
+            $table->foreignId('course_id')->constrained()->onDelete('cascade');
             $table->uuid('uuid');
             $table->string('name');
             $table->timestamps();
