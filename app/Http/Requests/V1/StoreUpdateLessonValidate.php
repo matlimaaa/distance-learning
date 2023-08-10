8f19797d81bf50e7e3330a92efec7cc8fe8e26c9
Mateus Lima de Freitas <dev.org.ml@gmail.com>
2023-08-09 21:50:11 -0300

    refactor: improve StoreUpdateLessonValidate class

--- app/Http/Requests/V1/StoreUpdateLessonValidate.php
+++ app/Http/Requests/V1/StoreUpdateLessonValidate.php
@@ -11,7 +11,7 @@ class StoreUpdateLessonValidate extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,21 @@ class StoreUpdateLessonValidate extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => [
+                'required',
+                'string',
+                'min:5',
+                'max:255'
+            ],
+            'video' => [
+                'required',
+                'string',
+            ],
+            'description' => [
+                'nullable',
+                'min:3',
+                'max:9999',
+            ],
         ];
     }
 }
