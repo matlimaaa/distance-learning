7e449ec96bc265d82b6754fea38440c3fecb551e
Mateus Lima de Freitas <dev.org.ml@gmail.com>
2023-08-12 19:39:53 -0300

    feat: add cache removal through observer in Lesson operations

--- app/Observers/LessonObserver.php
+++ app/Observers/LessonObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Lesson;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Str;
 
 class LessonObserver
@@ -13,5 +14,21 @@ class LessonObserver
     public function creating(Lesson $lesson): void
     {
         $lesson->uuid = strval(Str::uuid());
+    }
+
+    /**
+     * Handle the Course "updating" event.
+     */
+    public function updating(): void
+    {
+        Cache::forget('all_couses');
+    }
+
+    /**
+     * Handle the Course "deleting" event.
+     */
+    public function deleting(): void
+    {
+        Cache::forget('all_couses');
     }
 }
