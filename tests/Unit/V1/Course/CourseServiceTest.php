68721c99e2c53f57c43362392be0640555b6885e
Mateus Lima de Freitas <dev.org.ml@gmail.com>
2023-08-26 17:15:23 -0300

    test: add new tests to the CourseService

--- tests/Unit/V1/Course/CourseServiceTest.php
+++ tests/Unit/V1/Course/CourseServiceTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\V1\Course;
 
 use App\Models\Course;
 use App\Services\V1\Course\CourseServiceContract;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Collection;
 use Tests\TestCase;
@@ -53,5 +54,59 @@ class CourseServiceTest extends TestCase
 
         $this->assertInstanceOf(Course::class, $course);
         $this->assertEquals($course->id, $searchedCourse->id);
+    }
+
+    public function testDeleteACourseSuccessfullly(): void
+    {
+        $course = Course::factory()->create();
+
+        $this->courseService->destroy($course->uuid);
+
+        $this->assertSoftDeleted('courses', [
+            'id' => $course->id
+        ]);
+    }
+
+    public function testTryToDeleteACourseThatDoesNotExist(): void
+    {
+        $this->expectException(ModelNotFoundException::class);
+
+        $this->courseService->destroy($this->faker->uuid());
+    }
+
+    public function testUpdateCourseSuccessfullly(): void
+    {
+        $course = Course::factory()->create();
+
+        $newName = $this->faker->unique()->name();
+
+        $this->courseService->update(
+            $course->uuid,
+            [
+                'name' => $newName,
+                'description' => $this->faker->sentence(),
+            ]
+        );
+
+        $this->assertDatabaseMissing('courses', [
+            'name' => $course->name
+        ]);
+        
+        $this->assertDatabaseHas('courses', [
+            'name' => $newName
+        ]);
+    }
+
+    public function testTryToUpdateACourseThatDoesNotExist(): void
+    {
+        $this->expectException(ModelNotFoundException::class);
+
+        $this->courseService->update(
+            $this->faker->uuid(),
+            [
+                'name' => $this->faker->unique()->name(),
+                'description' => $this->faker->sentence(),
+            ]
+        );
     }
 }
