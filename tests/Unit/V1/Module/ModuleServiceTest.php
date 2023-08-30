944738adf067b1826c8269eec08c456fb3c6b92e
Mateus Lima de Freitas <dev.org.ml@gmail.com>
2023-08-29 21:14:00 -0300

    test: add tests to the ModuleService

---
+++ tests/Unit/V1/Module/ModuleServiceTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\Unit\V1\Module;
+
+use App\Models\Course;
+use App\Models\Module;
+use App\Services\V1\Module\ModuleServiceContract;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Collection;
+use Tests\TestCase;
+
+class ModuleServiceTest extends TestCase
+{
+    use WithFaker;
+
+    private Course $course;
+
+    private ModuleServiceContract $moduleService;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->course = Course::factory()->create();
+
+        $this->moduleService = app()->make(ModuleServiceContract::class);
+    }
+
+    public function testGetAllModules(): void
+    {
+        Module::factory(2)->for($this->course)->create();
+
+        $modules = $this->moduleService->getModulesByCourse($this->course->uuid);
+
+        $this->assertInstanceOf(Collection::class, $modules);
+
+        $modules->each(function ($module) {
+            $this->assertInstanceOf(Module::class, $module);
+        });
+    }
+
+    public function testNotFoundModule(): void
+    {
+        $this->expectException(ModelNotFoundException::class);
+
+        $this->moduleService->getModuleByCourse($this->course->uuid, $this->faker->uuid());
+    }
+
+    public function testGetModuleSuccessfully(): void
+    {
+        $module = Module::factory()->for($this->course)->create();
+
+        $foundedModule = $this->moduleService->getModuleByCourse($this->course->uuid, $module->uuid);
+        
+        $this->assertInstanceOf(Module::class, $foundedModule);
+
+        $this->assertEquals($module->id, $foundedModule->id);
+    }
+}
