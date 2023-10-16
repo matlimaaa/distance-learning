08a88f3b888642950d60718f3bf83d2e03a26258
Mateus Lima de Freitas <dev.org.ml@gmail.com>
2023-10-16 19:06:43 -0300

    test: create testGetAllCourses test from CourseRepository

---
+++ tests/Unit/V1/Course/CourseRepositoryTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Unit\V1\Course;
+
+use App\Models\Course;
+use App\Repositories\V1\Course\CourseRepositoryContract;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Cache;
+use Tests\TestCase;
+
+class CourseRepositoryTest extends TestCase
+{
+    use WithFaker;
+
+    private CourseRepositoryContract $courseRepository;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->courseRepository = app()->make(CourseRepositoryContract::class);
+    }
+
+    public function testGetAllCourses()
+    {
+        Course::factory(5)->create();
+
+        $courses = $this->courseRepository->getAllCourses();
+
+        $this->assertInstanceOf(Collection::class, $courses);
+        $this->assertCount(5, $courses);
+        $this->assertTrue(Cache::has('all_courses'));
+    }
+}
