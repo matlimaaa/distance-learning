5ddc187d727eacbfe49ab7703e8fcb91543dc4bd
Mateus Lima de Freitas <dev.org.ml@gmail.com>
2023-10-18 21:53:59 -0300

    test: create new tests in ModuleRepositoryTest class

--- tests/Unit/V1/Module/ModuleRepositoryTest.php
+++ tests/Unit/V1/Module/ModuleRepositoryTest.php
@@ -46,4 +46,79 @@ class ModuleRepositoryTest extends TestCase
 
         $this->moduleRepository->getModuleByCourse($this->course->id, $this->faker->uuid());
     }
+
+    public function testGetModuleSuccessfully(): void
+    {
+        $module = Module::factory()->for($this->course)->create();
+
+        $moduleFound = $this->moduleRepository->getModuleByCourse($this->course->id, $module->uuid);
+        
+        $this->assertInstanceOf(Module::class, $moduleFound);
+
+        $this->assertEquals($module->id, $moduleFound->id);
+    }
+    
+    public function testCreateANewModuleSucessfully(): void
+    {
+        $newModule = $this->moduleRepository->store(
+            $this->course->id,
+            [
+                'name' => $this->faker->name(),
+            ],
+        );
+
+        $this->assertInstanceOf(Module::class, $newModule);
+        $this->assertDatabaseHas('modules', [
+            'id' => $newModule->id
+        ]);
+    }
+
+    public function testUpdateModuleSuccessfullly(): void
+    {
+        $module = Module::factory()->for($this->course)->create();
+
+        $newName = str_shuffle($this->faker->name());
+
+        $successfullyUpdated = $this->moduleRepository->updateModule(
+            $this->course->id,
+            $module->uuid,
+            [
+                'name' => $newName,
+            ],
+        );
+
+        $this->assertTrue($successfullyUpdated);
+        $this->assertDatabaseMissing('modules', [
+            'name' => $module->name,
+        ]);
+        $this->assertDatabaseHas('modules', [
+            'name' => $newName,
+        ]);
+    }
+
+    public function testTryToUpdateAModuleThatDoesNotExist(): void
+    {
+        $this->expectException(ModelNotFoundException::class);
+
+        $this->moduleRepository->updateModule(
+            $this->course->id,
+            $this->faker->uuid(),
+            [
+                'name' => $this->faker->name(),
+            ],
+        );
+    }
+
+    public function testDeleteAModuleSuccessfullly(): void
+    {
+        $module = Module::factory()->for($this->course)->create();
+
+        $result = $this->moduleRepository->destroyModule($module->uuid);
+        
+        $this->assertTrue($result);
+        $this->assertSoftDeleted('modules', [
+            'id' => $module->id,
+            'name' => $module->name,
+        ]);
+    }
 }
